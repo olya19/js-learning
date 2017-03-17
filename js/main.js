ab6bdbaf184decae22e83bb9d211285ed65cd6de
olya19 <titarenko.olga.1997@gmail.com>
2017-03-17 22:00:20 +0200

    Changed css, added js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Olga on 17.03.2017.
+ */
+
+window.onload = function () {
+    var title = document.getElementById('page').querySelector('h1');
+    console.info(title.innerHTML);
+    title.innerHTML = 'Hello';
+    console.info(title.innerHTML);
+
+    setTimeout( function () {
+        console.info('title change');
+        title.style.backgroundColor = 'blue';
+        }, 2000);
+};
